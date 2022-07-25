8f663e8303d1b081c941e9afd1ba57d2185a2dd3
Sara Mohammadi <sara.mohammadi@emotion.com.ua>
2022-07-25 21:09:55 +0300

    update menu

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const navbarSocial = document.getElementsByClassName("navbar-social")[0];
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar");
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
-  navbarSocial.classList.toggle("active");
 });
